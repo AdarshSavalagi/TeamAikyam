f86418067f18b37126f5f157cf84c08cb8907c19
ADARSH <120472249+AdarshSavalagi@users.noreply.github.com>
2023-12-21 13:53:20 +0530

    Init Commit

---
+++ backend/core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import pandas as pd
+import joblib,os
+
+class CustomUser(AbstractUser):
+    level = models.IntegerField(blank=True)
+    income = models.IntegerField(default=0)
+    expense = models.IntegerField(default=0)
+    asset = models.IntegerField(default=0)
+    expense = models.IntegerField(default=0)
+    loan=models.IntegerField(default=0)
+    financial_goal = models.CharField(max_length=50)
+    def save(self,*args, **kwargs) -> None:
+        if self.pk:
+            # abishai model classify madi level na set madutte
+            loaded_model = joblib.load(os.path.join('ML','random_forest_classifier_model.pkl'))
+            user_data = pd.DataFrame([[self.asset,self.income, self.loan]], columns=['asset', 'income', 'loan amount'])
+            predicted_level = loaded_model.predict(user_data)
+            self.level=predicted_level[0]
+        return super().save(*args,**kwargs)
+
+
+class Investment(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.IntegerField(default=0)
+    description = models.TextField()
+    def save(self,*args,**kwargs):
+        # send notifications
+        super().save(*args,**kwargs)
+    def __str__(self) -> str:
+        return self.name
+
+class Insurance(models.Model):
+    name= models.CharField(max_length=50)
+    
